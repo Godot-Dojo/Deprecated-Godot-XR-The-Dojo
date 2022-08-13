495abb7ac88eb95cbaf65c23f1372597a57d984a
teddybear082 <87204721+teddybear082@users.noreply.github.com>
2022-08-13 07:53:24 -0400

    -add slicing katana and sliceable woodenman
    -add slicing katana
    -add sliceable woodenman
    -add kinematic body to katana for "push" collisions with other objects
    -adjust collision layers/masks of player physics hands and cube to enable more kinematic/physics interactions

---
+++ addons/slicer/slicer.gd
@@ -0,0 +1,23 @@
+tool
+extends Area
+
+
+var plane_point_a:Vector3
+var plane_point_b:Vector3
+var plane_point_c:Vector3
+
+
+	
+
+
+func _on_Area_body_entered(body):
+	pass
+	
+
+
+func _on_SlicingArea_body_exited(body):
+	if body is sliceable:
+		plane_point_a = $mesh/A.global_transform.origin
+		plane_point_b = $mesh/B.global_transform.origin
+		plane_point_c = $mesh/C.global_transform.origin
+		body.cut_object(Plane(plane_point_a,plane_point_b,plane_point_c))# Replace with function body.
