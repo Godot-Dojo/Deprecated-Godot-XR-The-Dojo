6cbe058bc4080cb7d207f2e3b34d26e33619af08
teddybear082 <87204721+teddybear082@users.noreply.github.com>
2022-08-16 11:50:41 -0400

    add alpha version of shadow avatar
    -known issue: shadow avatar scales too big

---
+++ addons/godot-xr-avatar/scripts/shadow.gd
@@ -0,0 +1,216 @@
+extends Spatial
+#This is an attempt to make a largely modular version of code created by SYBIOTE for the Oculus Toolkit created by NeoSparks314
+#for a player VRIK avatar.  This script should be added to the spatial root of the avatar object.
+
+#This code is NOT fully automated.  It does require some actions in the editor to work with the creation of some nodes.  It needs: 
+#(1) an AnimationTree node with proper animation blends set as a child of avatar and pointing to the character AnimationPlayer
+#(2) SkeletonIKL node set to Root Bone of left upper arm and Tip Bone set to Left Hand, Use Magnet, Magnet.x = 3, Magnet.y = -5, Magnet z = -10, [old value was just Magnet.x = 10] Interpolation set to 1  
+#(3) SkeletonIKR node set to Root Bone of Right Shoulder and Tip Bone set to Right Hand, Use Magnet, Magnet.x = -3, Magnet.y = -5, Magnet.z = -10, [old value was just Magnet.x = -10] Interpolation set to 1 
+#(4) SkeletonIKLegL node set to LeftUpLeg and Tip Bone set to LeftFoot, Use Magnet, Magnet (.2,0,1), Interpolation set to 1; 
+#(5) SkeletonIKLegR node set to RightUpLeg and Tip Bone set to RightFoot, Use Magnet, Magnet (-.2,0,1), Interpolation set to 1; 
+#(6) bone attachment for character_height set to top of avatar head/Head_Top_End
+#(7) bone attachment right_foot set to RightFoot; 
+#(8) bone attachment left_foot set to LeftFoot
+
+#set export variables to key elements of XR Rig necessary for avatar movement
+
+export (NodePath) var arvrorigin_path = null
+export (NodePath) var arvrcamera_path = null
+export (NodePath) var left_controller_path = null
+export (NodePath) var right_controller_path = null
+export (NodePath) var left_hand_path = null
+export (NodePath) var right_hand_path = null
+
+
+#export variables to fine tune avatar movement
+export var height_offset := 0.0
+export var foot_offset := 0.15
+export var ik_raycast_height := 2.0
+export var min_max_interpolation := Vector2(0.03, 0.9)
+export var smoothing := 0.3
+
+
+#Other variables needed for IK
+var max_height : float
+var avatar_height : float
+var prev_move := Vector2.ZERO
+
+
+#variables used for automatic creation of targets for IK and Raycasts
+var left_hand_target : Position3D = null
+var right_hand_target : Position3D = null
+var left_target : Position3D = null
+var right_target : Position3D = null
+var left_target_transform : Position3D = null
+var right_target_transform : Position3D = null
+var Raycast_L : RayCast = null
+var Raycast_R : RayCast = null
+var RL_dB := Basis.IDENTITY
+var LL_db := Basis.IDENTITY
+var armature_scale := Vector3.ONE
+
+#set all nodes 
+onready var arvrorigin = get_node(arvrorigin_path)
+onready var arvrcamera = get_node(arvrcamera_path)
+onready var left_controller = get_node(left_controller_path)
+onready var right_controller = get_node(right_controller_path)
+onready var left_hand = get_node(left_hand_path)
+onready var right_hand = get_node(right_hand_path)
+onready var left_foot : BoneAttachment = $Armature/Skeleton/left_foot
+onready var right_foot : BoneAttachment = $Armature/Skeleton/right_foot
+onready var LL_ik : SkeletonIK = $Armature/Skeleton/SkeletonIKLegL
+onready var RL_ik : SkeletonIK = $Armature/Skeleton/SkeletonIKLegR
+onready var skeleton : Skeleton = $Armature/Skeleton
+
+var avatar_height_set = false
+
+func _ready():
+		
+	#create left hand and right hand targets automatically 	
+	left_hand_target = Position3D.new()
+	left_hand_target.name = "left_target"
+	left_controller.add_child(left_hand_target, true)
+	left_hand_target.rotation_degrees.y = 90
+	left_hand_target.rotation_degrees.z = -90
+	
+	right_hand_target = Position3D.new()
+	right_hand_target.name = "right_target"
+	right_controller.add_child(right_hand_target, true)
+	right_hand_target.rotation_degrees.y = -90
+	right_hand_target.rotation_degrees.z = 90
+	
+	
+	# match avatar hands to XR Tools hands positions
+	left_controller.get_node("left_target").translation = left_hand.translation
+	right_controller.get_node("right_target").translation = right_hand.translation
+	
+	#Automatically generate other helper target nodes used in the IK
+	left_target = Position3D.new()
+	left_target.name = "LL_c"
+	add_child(left_target, true)
+	
+		#used when hooking to left leg SkeletonIK
+	left_target_transform = Position3D.new()
+	left_target_transform.name = "LL_t"
+	left_target.add_child(left_target_transform, true)
+		#match target rotations to bone attachment rotations which are avatar-specific
+	left_target_transform.rotation_degrees.x = left_foot.rotation_degrees.x + $Armature/Skeleton.rotation_degrees.x
+	left_target_transform.rotation_degrees.y = left_foot.rotation_degrees.y + $Armature/Skeleton.rotation_degrees.y
+	left_target_transform.rotation_degrees.z = left_foot.rotation_degrees.z + $Armature/Skeleton.rotation_degrees.z
+	
+	right_target = Position3D.new()
+	right_target.name = "RL_c"
+	add_child(right_target, true)
+	
+		#used when hooking to right leg SkeletonIK
+	right_target_transform = Position3D.new()
+	right_target_transform.name = "Rl_t"
+	right_target.add_child(right_target_transform, true)
+		#match target rotations to bone attachment rotations which are avatar-specific
+	right_target_transform.rotation_degrees.x = right_foot.rotation_degrees.x + $Armature/Skeleton.rotation_degrees.x
+	right_target_transform.rotation_degrees.y = right_foot.rotation_degrees.y + $Armature/Skeleton.rotation_degrees.y
+	right_target_transform.rotation_degrees.z = right_foot.rotation_degrees.z + + $Armature/Skeleton.rotation_degrees.z
+	
+	
+	#Set skeleton targets to the automatically generated target nodes
+	$Armature/Skeleton/SkeletonIKL.set_target_node(NodePath("../../../../" + left_controller.name + "/left_target"))
+	$Armature/Skeleton/SkeletonIKR.set_target_node(NodePath("../../../../" + right_controller.name + "/right_target"))
+	LL_ik.set_target_node(NodePath("../../../LL_c/LL_t"))
+	RL_ik.set_target_node(NodePath("../../../RL_c/Rl_t"))
+	
+	#set other used variables
+	RL_dB = left_target.transform.basis
+	LL_db = right_target.transform.basis
+	
+	#automatically generate Left and Right RayCast nodes used in IK movement
+	Raycast_L = RayCast.new()
+	Raycast_L.name = "RayCastL"
+	add_child(Raycast_L, true)
+	Raycast_L.enabled = true
+	Raycast_L.cast_to = Vector3(0,-4,0)
+	
+	Raycast_R = RayCast.new()
+	Raycast_R.name = "RayCastR"
+	add_child(Raycast_R, true)
+	Raycast_R.enabled = true
+	Raycast_R.cast_to = Vector3(0,-4,0)
+	
+	#start the IK
+	$Armature/Skeleton/SkeletonIKL.start()
+	$Armature/Skeleton/SkeletonIKR.start()
+	LL_ik.start()
+	RL_ik.start()
+	
+	
+
+		
+		
+func look_at_y(from: Vector3, to: Vector3, up_ref := Vector3.UP) -> Basis:
+	var forward := (to-from).normalized()
+	var right := up_ref.normalized().cross(forward).normalized()
+	forward = right.cross(up_ref).normalized()
+	return Basis(right, up_ref, forward)
+
+
+func update_ik_anim(target: Spatial, raycast: RayCast, bone_attach: BoneAttachment, d_b: Basis, avatar_height: float, hit_offset: float) -> void:
+	var bone_pos = bone_attach.global_transform.origin
+	raycast.global_transform.origin = bone_pos + Vector3.UP*avatar_height
+	target.global_transform.origin = bone_pos
+	raycast.global_transform.origin
+	target.global_transform.basis = d_b
+	var hit_point = raycast.get_collision_point().y + hit_offset
+	if raycast.is_colliding():
+		target.global_transform.origin.y = hit_point
+		if raycast.get_collision_normal() != Vector3.UP:
+			target.global_transform.basis = look_at_y(Vector3.ZERO,$Armature.global_transform.basis.z,raycast.get_collision_normal())
+	target.rotation.y = $Armature.rotation.y
+
+
+func _physics_process(delta) -> void:
+	# Set shadow avatar scale
+	if avatar_height_set == false:
+		avatar_height = $Armature/Skeleton/character_height.transform.origin.y
+		$Armature.scale *= get_parent().get_node("ARVRCamera").transform.origin.y / avatar_height
+		armature_scale = $Armature.scale
+		max_height = get_parent().get_node("ARVRCamera").transform.origin.y
+		avatar_height_set = true
+		
+	# Move the avatar under the camera and facing in the direction of the camera
+	var avatar_pos: Vector3 = arvrorigin.global_transform.xform(Plane.PLANE_XZ.project(arvrcamera.transform.origin))
+	var avatar_dir_z := Plane.PLANE_XZ.project(arvrcamera.global_transform.basis.z).normalized()
+	var avatar_dir_x := Vector3.UP.cross(avatar_dir_z)
+	$Armature.global_transform = Transform(avatar_dir_x, Vector3.UP, avatar_dir_z, avatar_pos)
+	$Armature.global_scale(armature_scale*self.scale) #without this, armature scale reverts to pre-scaling values
+	#print("Armature scale after physics process is:")
+	#print($Armature.scale)
+	
+	# Position the skeleton Y to adjust for the player height
+	skeleton.transform.origin.y = get_parent().get_parent().get_current_player_height() - avatar_height + height_offset
+   
+	# Rotate the head Y bone (look up/down)
+	var head = skeleton.get_bone_pose(skeleton.find_bone("head"))
+	var angles = arvrcamera.rotation
+	angles.x *= -1;angles.z *= -1
+	angles.y -= lerp_angle(angles.y,arvrcamera.rotation.y,delta)
+	head.basis = Basis(angles)
+	skeleton.set_bone_pose(skeleton.find_bone("head"),head)
+
+
+
+
+	# Calculate foot movement based on players actual ground-movement velocity
+	#var player_velocity := player_body.velocity# - player_body.ground_velocity
+	#player_velocity = $Armature.global_transform.basis.xform_inv(player_velocity)
+	#var move := Vector2(player_velocity.x, player_velocity.z)
+	
+	# Calculate foot movement based on playees requested ground-movement velocity
+	var move = get_parent().get_parent().get_player_body_velocity()
+
+	# Perform player movement animation
+	$AnimationTree.set("parameters/movement/blend_position",lerp(prev_move,move,smoothing))
+	$AnimationTree.set("parameters/Add2/add_amount", 1)
+	update_ik_anim(left_target,Raycast_L,left_foot,LL_db,ik_raycast_height,foot_offset)
+	update_ik_anim(right_target,Raycast_R,right_foot,RL_dB,ik_raycast_height,foot_offset)
+	LL_ik.interpolation = clamp(1,min_max_interpolation.x,min_max_interpolation.y)
+	RL_ik.interpolation = clamp(1,min_max_interpolation.x,min_max_interpolation.y)
+	prev_move = move
