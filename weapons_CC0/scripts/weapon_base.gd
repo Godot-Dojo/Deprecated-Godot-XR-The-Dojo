aa6c800ad9428e4cfb04d70601da207e0b01e849
D@re <56046022+DigitalN8m4r3@users.noreply.github.com>
2022-10-05 00:16:45 +0200

    added Trigger code to XR Weapons
    updated weapon_base.gd with code for rotating the trigger on the firearm weapons
    added Firearm Slide to other weapons as well such as the pistol_mk/automatic/ak
    fixed automatic guns not being set to automatic

--- weapons_CC0/scripts/weapon_base.gd
+++ weapons_CC0/scripts/weapon_base.gd
@@ -13,12 +13,15 @@ export var is_ShotGun : bool = false
 export var is_Bolt_Action : bool = false
 export var is_FlareGun : bool = false
 export var is_full_automatic : bool = false
-
+export(NodePath) var triggerPath # set in the inspector once
+onready var trigger = get_node(triggerPath)
 onready var mag_zone = $Mag_Zone
 var can_shoot : bool = true
 var bullet = null
 var casing = null
 var magazine_ammo : int = 0
+# Trigger - true/false
+var trigger_time = false
 
 # initial transform when grabbed -> used for recoil recovery
 var grabbed_transform: Transform = Transform(Basis.IDENTITY, Vector3.ZERO)
@@ -35,42 +38,44 @@ func _ready():
 
 func action():
 	emit_signal("action_pressed", self)
-	# Get audio node
-	var audio = owner.get_node("{}/audio".format([name], "{}"));
-	# Get effects node
-	var effect = owner.get_node("{}/effect".format([name], "{}"));
-	
-	# If shot timer has expired, and either magazine clip has bullets or gun has "one in the chamber", allow shooting
-	if can_shoot and (magazine_ammo >= 1 or current_ammo >= 1):
-		# First check if magazine has the ammo, and if so, subtract bullet from magazine ammo count and from actual magazine
-		if magazine_ammo >= 1:
-			magazine_ammo-= 1
-			current_ammo = magazine_ammo
-			var magazine = mag_zone.picked_up_object
-			if magazine != null:
-				magazine.get_node("Ammo").ammo = magazine_ammo
-		# If magazine doesn't have bullet, that means there must be "one in the chamber" so use up that bullet instead
-		else:
-			current_ammo -= 1
-		$GunSound.play(.4)
-		bullet = bullet_scene.instance()
-		get_owner().add_child(bullet)
-		bullet.global_transform = $BulletSpawnPoint.global_transform
-		bullet.linear_velocity = -bullet.global_transform.basis.z * bullet_speed
+	if can_shoot:
+		_trigger_down()
+		# Get audio node
+		var audio = owner.get_node("{}/audio".format([name], "{}"));
+		# Get effects node
+		var effect = owner.get_node("{}/effect".format([name], "{}"));
 		
-		#Expel casing unless a bolt action rifle or flare gun
-		if is_Bolt_Action == false and is_FlareGun == false:
-			casing = casing_scene.instance()
-			get_owner().add_child(casing)
-			casing.global_transform = $CasingSpawnPoint.global_transform
-			casing.apply_impulse(Vector3.DOWN, casing.global_transform.basis.y *.15)
-
+		# If shot timer has expired, and either magazine clip has bullets or gun has "one in the chamber", allow shooting
+		if magazine_ammo >= 1 or current_ammo >= 1:
+			# First check if magazine has the ammo, and if so, subtract bullet from magazine ammo count and from actual magazine
+			if magazine_ammo >= 1:
+				magazine_ammo-= 1
+				current_ammo = magazine_ammo
+				var magazine = mag_zone.picked_up_object
+				if magazine != null:
+					magazine.get_node("Ammo").ammo = magazine_ammo
+			# If magazine doesn't have bullet, that means there must be "one in the chamber" so use up that bullet instead
+			else:
+				current_ammo -= 1
+			$GunSound.play(.4)
+			bullet = bullet_scene.instance()
+			get_owner().add_child(bullet)
+			bullet.global_transform = $BulletSpawnPoint.global_transform
+			bullet.linear_velocity = -bullet.global_transform.basis.z * bullet_speed
+			
+			#Expel casing unless a bolt action rifle or flare gun
+			if is_Bolt_Action == false and is_FlareGun == false:
+				casing = casing_scene.instance()
+				get_owner().add_child(casing)
+				casing.global_transform = $CasingSpawnPoint.global_transform
+				casing.apply_impulse(Vector3.DOWN, casing.global_transform.basis.y *.15)
+			# Trigger - Set Rotation to false
+			trigger_time = false
+			recoil()
 		# Make player wait until time designated for gun to allow next shot
 		can_shoot = false
 		$ShotTimer.wait_time = shot_timer_time
 		$ShotTimer.start()
-
-		recoil()
 
 func recoil(): 
 	# apply recoil transform 
@@ -87,8 +92,26 @@ func recoil_recover():
 
 #When shot timer expires, allow shot again
 func _on_ShotTimer_timeout():
+	# Trigger Rotation - CHECK
+	if !trigger_time:
+		_trigger_up()
+	_trigger_reset()
 	can_shoot = true
 	
+# > Trigger Rotation - START
+func _trigger_reset():
+	trigger.transform.basis = trigger.transform.basis.rotated(Vector3(1,0,0), 0.00)
+	# Trigger - Set Rotation to true
+	trigger_time = true
+	
+func _trigger_down():
+		# failsafe to stop trigger from rotating
+		trigger.transform.basis = trigger.transform.basis.rotated(Vector3(-1,0,0), 0.3)
+		trigger.transform.basis = trigger.transform.basis.rotated(Vector3(1,0,0), 0.00)
+		trigger_time = false
+func _trigger_up():
+	trigger.transform.basis = trigger.transform.basis.rotated(Vector3(1,0,0), 0.3)
+# < Trigger END
 
 func _on_Mag_Zone_has_picked_up(what):
 	if what.get_node_or_null("Ammo") != null:
